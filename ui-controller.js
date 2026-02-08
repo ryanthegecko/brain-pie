52b48f1d287fbcc683bb216128014e79c6106d47
Ryan Gecko <ryan@Ryans-MacBook-Pro-2.local>
2026-02-08 21:54:57 +0000

    Make text more likely to be white over colours

--- ui-controller.js
+++ ui-controller.js
@@ -773,7 +773,7 @@ const UI = {
         const g = parseInt(hexColor.substr(3, 2), 16);
         const b = parseInt(hexColor.substr(5, 2), 16);
         const brightness = (r * 299 + g * 587 + b * 114) / 1000;
-        return brightness < 128;
+        return brightness < 170;
     },
     
     renderCategoriesList(categories) {
